8f10ed035611ff22c121dd3c9ac246706009b6a4
sfkara <sfkara@anadolu.edu.tr>
2019-12-23 00:04:56 +0300

    first commit

---
+++ employee_register/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Employee
+
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = ('fullname', 'emp_code', 'mobile', 'position')
+        labels = {
+            'fullname': 'FULL NAME',
+            'emp_code': 'EMP. Code'
+
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(EmployeeForm, self).__init__(*args, **kwargs)
+        self.fields['position'].empty_label = 'Select'
+        self.fields['emp_code'].required = False
